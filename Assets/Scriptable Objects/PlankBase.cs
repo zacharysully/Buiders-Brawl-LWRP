a3e5d598ef1070f9d1f0347aca26fde4ec64186a
Timothy Robinson <timothyr218@gmail.com>
2019-09-09 23:20:23 -0700

    Created Plank Base Scriptable Object Script
    - Created PlankBase Scriptable Object Script
    - Created 3 new PlankBase Assets: Small, Medium, Large

---
+++ Assets/Scriptable Objects/PlankBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New PlankBase", menuName = "Plank Base", order = 1)]
+public class PlankBase : ScriptableObject
+{
+    [Tooltip("The movement of the player while holding this plank")]
+    [SerializeField]
+    private float playerMovementSpeed;
+
+    public float PlayerMovementSpeed
+    {
+        get
+        {
+            return playerMovementSpeed;
+        }
+    }
+}
